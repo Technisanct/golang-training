5db8667a1fb09060b35eb1b70913aa3f309b18da
Anisha J <anisha.j@technisanct.com>
2025-10-09 11:11:07 +0530

    updated

--- handler/handler.go
+++ handler/handler.go
@@ -1,17 +1,14 @@
 package handler
 
 import (
-	"github.com/gin-gonic/gin"
-	"golang-training/handler/category"
 	"golang-training/handler/ping"
-	"golang-training/handler/product"
 	"golang-training/handler/user"
+
+	"github.com/gin-gonic/gin"
 )
 
 // InitPublicRoutes ... routes without auth
 func InitPublicRoutes(router *gin.Engine) {
 	ping.AddRoute(router, "/ping")
 	user.AddRoute(router, "/user")
-	category.AddRoute(router, "/category")
-	product.AddRoute(router, "/product")
 }
