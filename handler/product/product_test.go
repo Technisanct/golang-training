ed38acabbb6470fc431384db9bbf60d0b801a2ed
Rohit <rohit@Admins-MacBook-Air.local>
2025-10-22 10:37:00 +0530

    NG-5: Add list product endpoint

--- handler/product/product_test.go
+++ handler/product/product_test.go
@@ -43,7 +43,7 @@ var (
 	}
 	expectedListProductResponse = ListProductResponse{
 		Message: "successful",
-		Data:    toHandlerProductMapping(returnListProductData),
+		Data:    mapLogicToHandler(returnListProductData),
 	}
 	expectedErrorListProductResponse = ListProductResponse{
 		Message: "",
--- logic/product/list.go
+++ logic/product/list.go
@@ -16,20 +16,24 @@ func (ps *productImpl) List(c context.Context) ([]*contract.Product, error) {
 		return nil, err
 	}
 
-	return toLogicProductMapping(products), nil
+	return mapRepoToLogic(products), nil
 }
 
-func toLogicProductMapping(input []*model.Product) []*contract.Product {
-	result := make([]*contract.Product, len(input))
-	for idx, product := range input {
-		result[idx] = &contract.Product{
-			ID:              product.ID.Hex(),
-			Name:            product.Name,
-			Price:           product.Price,
-			DiscountedPrice: product.DiscountedPrice,
-			CreatedAt:       product.CreatedAt,
-			UpdatedAt:       product.UpdatedAt,
+func mapRepoToLogic(input []*model.Product) []*contract.Product {
+	results := make([]*contract.Product, 0, len(input))
+	for _, product := range input {
+		if product != nil {
+			result := &contract.Product{
+				ID:              product.ID.Hex(),
+				Name:            product.Name,
+				Price:           product.Price,
+				DiscountedPrice: product.DiscountedPrice,
+				CreatedAt:       product.CreatedAt,
+				UpdatedAt:       product.UpdatedAt,
+			}
+			results = append(results, result)
 		}
 	}
-	return result
+
+	return results
 }
